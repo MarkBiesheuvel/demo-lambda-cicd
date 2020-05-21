bc9a220bbe2423be80f1ecb4b92aebf636dcb98f
Mark Biesheuvel <mail@markbiesheuvel.nl>
2020-05-21 10:25:51 +0200

    Add HTTP API

--- source/index.py
+++ source/index.py
@@ -1,11 +1,19 @@
 from arithmetic import add
 
-def lambda_handler(event, context):
-    outcome = add(event['a'], event['b'])
 
-    print('Outcome: {}'.format(outcome))
+def lambda_handler(event, context):
+    parameters = event.get('queryStringParameters', {})
+
+    a = int(parameters.get('a', 0))
+    b = int(parameters.get('b', 0))
+
+    total = add(a, b)
+
+    print('A: {}'.format(a))
+    print('B: {}'.format(b))
+    print('Total: {}'.format(total))
 
     return {
         'statusCode': 200,
-        'body': outcome
+        'body': total
     }
